fe4b086a4665d0fb7ce24383a88159065f33f1ce
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-07-02 09:07:15 -0400

    Add a post Model to resemble a Post

--- project4/network/models.py
+++ project4/network/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users_posts")
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"Post id: {self.id} by {self.author}"
