cc5e3e27cba8093a6c689bf2c64d0b627ee5c3c1
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-07-03 08:08:42 -0400

    F03: Add path for urls.py where username links to user's profile

--- project4/network/urls.py
+++ project4/network/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("newPost", views.new_post, name="new_post")
+    path("newPost", views.new_post, name="new_post"),
+    path("profile/<int:id>", views.visit_profile, name='visit_profile')
 ]
