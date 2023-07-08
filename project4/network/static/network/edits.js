2e3ff16d75656d430ff5cdaacc27c2a3387cb243
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-07-08 06:50:13 -0400

    F07: Add liking and unliking javascript frontend logic to perform actions without refreshing the page

--- project4/network/static/network/edits.js
+++ project4/network/static/network/edits.js
@@ -40,7 +40,7 @@ function edit(){
                     body: JSON.stringify({
                         new_content: new_content
                     })
-                })
+                }).then(response => response.json())
             }
 
         }
@@ -51,3 +51,61 @@ function edit(){
 document.addEventListener('DOMContentLoaded', function() {
   edit();
 })
+
+
+function subtract_indatabase(id,person_id){
+        function gettoken(name) {
+            const value = `;${document.cookie}`;
+            const parts = value.split(`;${name}=`);
+            if (parts.length == 2) return parts.pop().split(';').shift();
+        }
+
+    fetch(`/remove_like/${id}`,{
+        method: "POST",
+        headers: {"Content-type":"application/json", "X-CSRFToken":gettoken("csrftoken")},
+        body: JSON.stringify({
+            person_id: person_id
+        })
+    }).then(response => response.json())
+}
+
+
+function add_indatabase(id,person_id){
+        function gettoken(name) {
+            const value = `;${document.cookie}`;
+            const parts = value.split(`;${name}=`);
+            if (parts.length == 2) return parts.pop().split(';').shift();
+        }
+
+    fetch(`/add_like/${id}`,{
+        method: "POST",
+        headers: {"Content-type":"application/json", "X-CSRFToken":gettoken("csrftoken")},
+        body: JSON.stringify({
+            person_id: person_id
+        })
+    }).then(response => response.json())
+}
+
+function mySwitch(div) {
+    let likes = div.parentElement.querySelector("#num-of-likes").innerHTML;
+    let id = div.parentElement.querySelector("#id").innerHTML;
+    let person_id = div.parentElement.querySelector("#person-id").innerHTML;
+    let likes_number = parseInt(likes);
+
+    if (div.innerHTML === `<i class="fa-solid fa-heart fa-xl" style="color: #7F7CFF;"></i>`) {
+        likes_number = likes_number - 1;
+        div.innerHTML = `<i class="fa-regular fa-heart fa-xl" style="color: #7F7CFF;"></i>`;
+        div.parentElement.querySelector("#num-of-likes").innerHTML = likes_number.toString();
+        // to actually effect the database
+        subtract_indatabase(id,person_id);
+    }
+    else if (div.innerHTML === `<i class="fa-regular fa-heart fa-xl" style="color: #7F7CFF;"></i>`) {
+            likes_number = likes_number + 1;
+            div.innerHTML = `<i class="fa-solid fa-heart fa-xl" style="color: #7F7CFF;">`
+            div.parentElement.querySelector("#num-of-likes").innerHTML = likes_number.toString();
+            // to actually effect the database
+            add_indatabase(id,person_id);
+        }
+
+
+}
