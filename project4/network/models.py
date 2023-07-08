86efbc6ebdaa70cfcbd95890009e3cfe7d87560c
Beshoy-Hezky <hezkyb@mcmaster.ca>
2023-07-08 05:39:16 -0400

    F07: Change likes to be changed from POST many to many field to user many to many field

--- project4/network/models.py
+++ project4/network/models.py
@@ -7,6 +7,7 @@ class User(AbstractUser):
 
 
 class Post(models.Model):
+    user_liked = models.ManyToManyField('User', blank=True, related_name='posts_liked')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users_posts")
     timestamp = models.DateTimeField(auto_now_add=True)
     content = models.CharField(max_length=200)
